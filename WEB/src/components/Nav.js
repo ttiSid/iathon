645f387f4c73fc94a39284806f39c799a1f21aaa
maikoncorrea <maikonacorrea@gmail.com>
2023-09-23 19:00:42 -0300

    added css and html logo

--- WEB/src/components/Nav.js
+++ WEB/src/components/Nav.js
@@ -1,10 +1,12 @@
-
+import logo from "../images/logo.png"
 
 function Nav() {
     return(
-        <>
-        <h1>Navegação</h1>
-        </>
+        <nav className="nav">
+        <div className="nav__container">
+            <img className="nav__image" src={logo} alt="logo do site IATHON IA no diabetes"></img>
+        </div>
+        </nav>
     )
 }
 
