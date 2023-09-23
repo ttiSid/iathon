3545268496bffc619a7cb68beb3490160c541907
maikoncorrea <maikonacorrea@gmail.com>
2023-09-23 19:01:08 -0300

    html content

--- WEB/src/components/Header.js
+++ WEB/src/components/Header.js
@@ -2,9 +2,16 @@
 
 function Header() {
     return(
-        <>
-        <h1>Header</h1>
-        </>
+        <header className="header">
+            <div className="header__container">
+                <div className="header__discription">
+                    <h1>O uso da IA no controle e diagnóstico do Diabetes</h1>
+                    <p>Especificamente na esfera do diabetes (doença de incidência crescente que atinge 8,8% da população mundial e já representa 10% do gasto global em saúde), dados de bases populacionais, prontuários eletrônicos ou dispositivos vestíveis ou de monitorização contínua da glicemia podem fornecer substrato para algoritmos que executem diagnósticos mais precoces e acurados</p>
+                    <a>Saiba mais...</a>
+                </div>
+                <img className="header__image" src="" alt=""></img>
+            </div>
+        </header>
     )
 }
 
