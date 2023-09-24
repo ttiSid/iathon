ab14e5299eeea2792f739271283ecd0a4ed065dc
maikoncorrea <maikonacorrea@gmail.com>
2023-09-24 18:22:07 -0300

    future component test and css configured

---
+++ WEB/src/contains/Test.js
@@ -0,0 +1,16 @@
+
+function Test() {
+  return(
+    <section className="test">
+      <div className="test__text-container">
+        <h2 className="test__title">Faça seu teste aqui!</h2>
+        <p className="test__paragraph">Este teste tem como base, dados coletados do ....... , não serve como diagnóstico médico, para maiores esclarecimentos, consulte o seu médico, ou se dirija até o agente de saúde mais próximo.</p>
+      </div>
+      <div className="test__container">
+        <div className="test__input-container"></div>
+      </div>
+    </section>
+  )
+}
+
+export default Test;
