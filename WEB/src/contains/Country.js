37c764b40a4dc272076e7c31cfb1720c15a92bd5
maikoncorrea <maikonacorrea@gmail.com>
2023-09-24 16:40:42 -0300

    future component and css country

---
+++ WEB/src/contains/Country.js
@@ -0,0 +1,26 @@
+
+function Country() {
+  return(
+    <section className="country">
+      <h2 className="country__title">No Brasil</h2>
+      <p className="country__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquat enim ad minim.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquat enim ad minim.</p>
+      <ul className="country__list">
+        <li className="country__item">
+          <h4 className="country__item-percentage">83%</h4>
+          <p className="country__text">User Growth</p>
+        </li>
+        <li className="country__item">
+          <h4 className="country__item-percentage">90%</h4>
+          <p className="country__text">Active users</p>
+        </li>
+        <li className="country__item">
+          <h4 className="country__item-percentage">80%</h4>
+          <p className="country__text">Paid users</p>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+
+export default Country;
