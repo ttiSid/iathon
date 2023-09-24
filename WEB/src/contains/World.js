1dfd12ad0f8c2a97b4d4d51dbfa76926696bc092
maikoncorrea <maikonacorrea@gmail.com>
2023-09-24 04:32:59 -0300

    feature created world css /html/js

---
+++ WEB/src/contains/World.js
@@ -0,0 +1,46 @@
+
+function World() {
+  return (
+    <section className="world">
+      <h2 className="world__title">Números do Diabetes no Mundo</h2>
+      <p className="world__paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliquat enim ad minim. Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliquat enim ad minim.
+      </p>
+      <ul className="world__list">
+      <li className="world__item">
+          <img className="world__item-image" src="" alt=""></img>
+          <div className="world__item-container">
+            <h4 className="world__item-title">102</h4>
+            <p className="world__item-paragraph">Type something</p>
+          </div>
+        </li>
+        <li className="world__item">
+          <img className="world__item-image" src="" alt=""></img>
+          <div className="world__item-container">
+            <h4 className="world__item-title">102</h4>
+            <p className="world__item-paragraph">Type something</p>
+          </div>
+        </li>
+        <li className="world__item">
+          <img className="world__item-image" src="" alt=""></img>
+          <div className="world__item-container">
+            <h4 className="world__item-title">102</h4>
+            <p className="world__item-paragraph">Type something</p>
+          </div>
+        </li>
+        <li className="world__item">
+          <img className="world__item-image" src="" alt=""></img>
+          <div className="world__item-container">
+            <h4 className="world__item-title">102</h4>
+            <p className="world__item-paragraph">Type something</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default World;
